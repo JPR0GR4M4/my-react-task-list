17c6079aa105071a3ca9cb3b791ae4b541b789dc
Juan Pablo Caro <125841812+JPR0GR4M4@users.noreply.github.com>
2023-06-27 17:08:18 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+import useTaskList from "./hooks/useTaskList";
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen font-inter h-full text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Header />
+        <TaskList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="text-5xl font-anton font-bold italic tracking-widest">
+      <u>Todo App</u>
+    </h1>
+  );
+}
+
+export default Header;
